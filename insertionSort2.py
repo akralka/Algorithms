358f516af5d1bd5cc3447cfd1a562388f43868b2
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-08-08 20:58:16 +0200

    Update insertionSort2.py

--- insertionSort2.py
+++ insertionSort2.py
@@ -1,4 +1,3 @@
-
 def insertionSort2(n, arr):
     for i in range(1, len(arr)):
         num = arr[i]
@@ -7,15 +6,14 @@ def insertionSort2(n, arr):
             arr [j+1] = arr[j]
             j -= 1
         arr[j+1] = num
-        print(*arr)
+        print(*arr) #  Each step
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    n = int(input().strip())  # how many numbers in array
 
     arr = list(map(int, input().rstrip().split()))
 
     insertionSort2(n, arr)
-
 
 
 
