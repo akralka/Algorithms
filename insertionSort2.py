c17d345eb8c84151efc75d91d417e6e6f16dfb0e
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-08-08 21:01:41 +0200

    Update insertionSort2.py

--- insertionSort2.py
+++ insertionSort2.py
@@ -1,3 +1,18 @@
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         j = i
+#         while j > 0 and arr[j] <= arr[j-1]:
+#             arr[j], arr[j-1] = arr[j-1], arr[j]
+#             j -= 1
+#     print(arr)
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+
+#     arr = list(map(int, input().rstrip().split()))
+#     insertion_sort(arr)
+    
+--------------------------------------------------------------------------------------------------
 def insertionSort2(n, arr):
     for i in range(1, len(arr)):
         num = arr[i]
